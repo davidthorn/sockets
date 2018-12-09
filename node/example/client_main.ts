b50027afbd44a3e354c0bc3c926fbbf09d9dfe93
David Thorn <david.thorn221278@googlemail.com>
2018-12-09 18:20:49 +0100

    renamed the example files to server and client

--- node/example/client_main.ts
+++
@@ -1,6 +0,0 @@
-import SocketClient from '../src/SocketClient'
-import { port, host } from '../server.json'
-
-let socketClient = new SocketClient(host, port)
-socketClient.connect( client => {
-})
--- node/example/server.js
+++ node/example/server.js
@@ -5,4 +5,4 @@ const Server_1 = require("../src/Server");
 const server_json_1 = require("../server.json");
 const server = new Server_1.default(server_json_1.host, server_json_1.port);
 server.listen();
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=server.js.map
